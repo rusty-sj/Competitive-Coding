df22a073169a8e2fdcde134b84afae09e005ed6a
Rashmi Jadhav <rsjcoepit@gmail.com>
2020-12-30 12:55:47 +0530

    Time: 3 ms (99.23%), Space: 55.4 MB (30.86%) - LeetHub

--- best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        // TC: O(n)
+        // SC: O(1)
         int minCost1 = Integer.MAX_VALUE, minCost2 = Integer.MAX_VALUE;
         int profit1 = 0, profit2 = 0;
-        for (int i = 0; i < prices.length; i++) {
+        for (int price: prices) {
+            // Transaction 1
+            minCost1 = Math.min(minCost1, price);
+            profit1 = Math.max(profit1, price - minCost1);
             
-            // Trasaction 1: buy only if price is less than min cost till now
-            if (prices[i] < minCost1) 
-                minCost1 = prices[i];
-            else {
-            // Trasaction 1: sell if profit by selling prices[i] is more than before
-                if ((prices[i] - minCost1) > profit1) 
-                    profit1 = prices[i] - minCost1;
-            }
-            // Trasaction 2: buy only if price is less than min cost till now
-            if (prices[i] - profit1 < minCost2) 
-                minCost2 = prices[i] - profit1;
-            else {
-            // Trasaction 2: sell if profit by selling prices[i] is more than before
-                if (prices[i] - minCost2 > profit2) 
-                    profit2 = prices[i] - minCost2;
-            }
-            
+            // Transaction 2
+            minCost2 = Math.min(minCost2, price - profit1);
+            profit2 = Math.max(profit2, price - minCost2);
         }
         return profit2;
     }
