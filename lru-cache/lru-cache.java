11c17d31158802f43c98cb5e4e7da41cd6884f9d
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-03-23 20:04:23 -0700

    Time: 13 ms (81.81%), Space: 47.3 MB (56.24%) - LeetHub

--- lru-cache/lru-cache.java
+++ lru-cache/lru-cache.java
@@ -1,31 +1,73 @@
+class DLLNode {
+    int key;
+    int value;
+    DLLNode prev = null;
+    DLLNode next = null;
+    
+    public DLLNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
 class LRUCache {
 
     int capacity;
-    Map<Integer, Integer> map;
+    Map<Integer, DLLNode> cache;
+    DLLNode head, tail;
     
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.map = new LinkedHashMap<Integer, Integer>();
+        this.cache = new HashMap<>();
+        this.head = new DLLNode(-1, -1);
+        this.tail = new DLLNode(-1, -1);
+        this.head.next = tail;
+        this.tail.prev = head;
     }
     
     public int get(int key) {
-        int value = this.map.getOrDefault(key, -1);
-        if (value != -1) {
-            this.map.remove(key);
-            this.map.put(key, value);
+        int returnVal = -1;
+        DLLNode node = this.cache.getOrDefault(key, null);
+        if (node != null) {
+            returnVal = node.value;
+            remove(node);
+            addFront(node);
         }
-        return value;
+        return returnVal;
     }
     
     public void put(int key, int value) {
-        int val = this.map.getOrDefault(key, -1);
-        if (val == -1) {
-            if (this.map.size() == this.capacity) {
-                this.map.remove(this.map.keySet().iterator().next());
+        DLLNode node = this.cache.getOrDefault(key, null);
+        if (node == null) {
+            node = new DLLNode(key, value);
+            if (this.cache.size() == this.capacity) {
+                DLLNode last = this.tail.prev;
+                this.cache.remove(last.key);
+                remove(last);
             }
-        } else
-            this.map.remove(key);
-        this.map.put(key, value);
+        } else {
+            node.value = value;
+            remove(node);
+        }
+        addFront(node);
+        this.cache.put(key, node);
+    }
+    
+    /* Doubly Linked List utils */
+    
+    // Remove a node from the linked list
+    public void remove(DLLNode node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    // Add given node to the beginning of the linked list
+    public void addFront(DLLNode node) {
+        DLLNode headNext = this.head.next;
+        this.head.next = node;
+        node.next = headNext;
+        node.prev = this.head;
+        node.next.prev = node;
     }
 }
 
