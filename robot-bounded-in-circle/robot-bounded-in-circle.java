6278ad18d31f23e7242fa2e48307974de2ad37f7
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-03-24 08:53:33 -0700

    Time: 0 ms (100.00%), Space: 37.1 MB (64.15%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.java
+++ robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -1,19 +1,29 @@
 class Solution {
     public boolean isRobotBounded(String instructions) {
-        int x = 0, y = 0;
-        int[][] dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int anchor = 0;
-        for (int i = 0; i < instructions.length(); i++) {
-            char instruction = instructions.charAt(i);
-            if (instruction == 'G') {
-                x += dirs[anchor][0];
-                y += dirs[anchor][1];
-            } else if (instruction == 'L') {
-                anchor = (anchor + 3) % 4;
-            } else if (instruction == 'R') {
-                anchor = (anchor + 1) % 4;
+        
+        // Directions in sequence N, E, S, W
+        int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        
+        // Start at position (0, 0) and direction dirs[0]/North
+        int coordinateX = 0;
+        int coordinateY = 0;
+        int direction = 0; // Ranges 0-3
+        
+        for (char c: instructions.toCharArray()) {
+            if (c == 'G') {
+                coordinateX += dirs[direction][0];
+                coordinateY += dirs[direction][1];
+            } else if (c == 'L') {
+                direction = (direction + 3) % 4;
+            } else if (c == 'R') {
+                direction = (direction + 1) % 4;
             }
         }
-        return (x == 0 && y == 0) || (anchor != 0);
+        
+        // If we end up at start position (0, 0) or we end up in a direction other than start direction (N), there is a circle
+        if ((coordinateX == 0 && coordinateY == 0) || direction != 0)
+            return true;
+        
+        return false;
     }
 }
