9ca291cb9f499996bd32d35677f1ac9599e7bf9e
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-03-25 11:51:05 -0700

    Time: 0 ms (100.00%), Space: 39.3 MB (40.37%) - LeetHub

--- subsets/subsets.java
+++ subsets/subsets.java
@@ -1,6 +1,7 @@
 class Solution {
     
     List<List<Integer>> output;
+    
     public List<List<Integer>> subsets(int[] nums) {
         output = new ArrayList<>();
         recurse(nums, 0, new ArrayList<Integer>());
@@ -8,15 +9,18 @@ class Solution {
     }
     
     public void recurse(int[] nums, int index, List<Integer> list) {
+        // A subset is found when we have covered all indices in input
         if (index == nums.length) {
             output.add(list);
             return;
         }
         
+        // Do not include element at index: Create copy to avoid referencing issue
         recurse(nums, index + 1, new ArrayList<Integer>(list));
+        
+        // Include element at index: Create copy to avoid referencing issue
         List<Integer> temp = new ArrayList<Integer>(list);
         temp.add(nums[index]);
         recurse(nums, index + 1, temp);
-        
     }
 }
