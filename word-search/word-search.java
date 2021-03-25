a75d87bb02c8563fe0d0962fc43ed68bb21fefff
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-03-24 23:17:38 -0700

    Time: 119 ms (11.29%), Space: 41.2 MB (37.05%) - LeetHub

---
+++ word-search/word-search.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    int[][] directions = new int[][]{{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+    boolean[][] visited;
+    int n, m;
+    
+    public boolean exist(char[][] board, String word) {
+        n = board.length;
+        m = board[0].length;
+        visited = new boolean[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // Call backtrack if start of word is found
+                if (board[i][j] == word.charAt(0)) {
+                    visited[i][j] = true;
+                    if (backtrack(board, i, j, word, 1))
+                        return true;
+                    visited[i][j] = false;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean backtrack(char[][] board, int i, int j, String word, int indexWord) {
+        if (indexWord == word.length())
+            return true;
+        
+        for (int[] direction: directions) {
+            visited[i][j] = true;
+            int row = i + direction[0];
+            int column = j + direction[1];
+            if (row >= 0 && row < board.length
+               && column >= 0 && column < board[i].length
+               && visited[row][column] == false
+               && word.charAt(indexWord) == board[row][column])
+                if(backtrack(board, row, column, word, indexWord + 1))
+                    return true;
+            visited[i][j] = false;
+        }
+        return false;
+    }
+    
+}
