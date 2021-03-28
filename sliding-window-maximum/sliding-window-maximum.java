253de7fd7346580374e888f1dd130de121f14156
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-03-27 23:49:25 -0700

    Time: 77 ms (13.06%), Space: 60.4 MB (16.65%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -1,24 +1,33 @@
 class Solution {
+    // Time Complexity: O(N log N) : worst case heap can grow to size n
+    // Space Complexity: O(N)
     public int[] maxSlidingWindow(int[] nums, int k) {
         int[] output = new int[nums.length - k + 1];
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        
+        // Pair of value and index
+        // PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[0] == b[0] ? a[1] - b[1] : b[0] - a[0]);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> b[0] - a[0]);
         int left = 0, right = 0;
         
         // Add first interval 
         while (right < k) {
             queue.add(new int[]{nums[right] , right});
-            // System.out.println(nums[right] + " " + right);
             right++;
         }
+        
+        // Update first max
         output[left++] = queue.peek()[0];
         
-        while (right < nums.length) {
-            queue.add(new int[]{nums[right] , right});
+        while (right < nums.length) {                   // O(N log N))
+            // Include new element at right index
+            queue.add(new int[]{nums[right] , right});  // O(log N)
+            
+            // If current maximum is out of the current interval, pop it
             while (queue.peek() != null && queue.peek()[1] < left) {
-                System.out.println(queue.peek()[1]);
-                System.out.println(left + " " + right);
-                queue.poll();
+                queue.poll();                           // O(log N)
             }
+            
+            // Update max for interval
             output[left] = queue.peek()[0];
             left++;
             right++;
