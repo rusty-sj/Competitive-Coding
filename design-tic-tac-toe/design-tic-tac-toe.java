345bbba5746f5df34706d31f2bddedd947f10cf2
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-04-08 23:10:54 -0700

    Time: 3 ms (100.00%), Space: 41.9 MB (61.92%) - LeetHub

--- design-tic-tac-toe/design-tic-tac-toe.java
+++ design-tic-tac-toe/design-tic-tac-toe.java
@@ -1,17 +1,14 @@
 class TicTacToe {
 
-    int n;
-    int[] rows;
-    int[] cols;
-    int diagonal1;
-    int diagonal2;
+    int[] row, col;
+    int diag1, diag2, n;
     /** Initialize your data structure here. */
     public TicTacToe(int n) {
+        this.row = new int[n];
+        this.col = new int[n];
         this.n = n;
-        this.rows = new int[n];
-        this.cols = new int[n];
-        this.diagonal1 = 0;
-        this.diagonal2 = 0;
+        this.diag1 = 0;
+        this.diag2 = 0;
     }
     
     /** Player {player} makes a move at ({row}, {col}).
@@ -23,17 +20,28 @@ class TicTacToe {
                 1: Player 1 wins.
                 2: Player 2 wins. */
     public int move(int row, int col, int player) {
-        int play = (player == 1 ? 1 : -1);
-        this.rows[row] += play;
-        this.cols[col] += play;
-        if (row == col)
-            this.diagonal1 += play;
-        if (row == (this.n - col - 1))
-            this.diagonal2 += play;
+        // Place 1 for player 1, -1 for player 2
+        int value = player == 1 ? +1 : -1;
         
-        if (Math.abs(this.rows[row]) == this.n || Math.abs(this.cols[col]) == this.n || Math.abs(this.diagonal1) == this.n || Math.abs(this.diagonal2) == this.n)
+        // row col placement
+        this.row[row] += value;
+        this.col[col] += value;
+        
+        // diag and reverse diag placement
+        if (row == col) 
+            this.diag1 += value;
+        if (row == (n - col - 1))
+            this.diag2 += value;
+        
+        // check if any diag was formed
+        if (Math.abs(diag1) == this.n || Math.abs(diag2) == this.n)
             return player;
-         return 0;
+        
+        // check if vert or horiz line was formed
+        if (Math.abs(this.row[row]) == this.n || Math.abs(this.col[col]) == this.n)
+            return player;
+    
+        return 0;
         
     }
 }
