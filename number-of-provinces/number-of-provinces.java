6458393515984176b8cd52ce938aa1eabab21cab
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-04-10 11:25:25 -0700

    Time: 1 ms (67.87%), Space: 40 MB (41.23%) - LeetHub

--- number-of-provinces/number-of-provinces.java
+++ number-of-provinces/number-of-provinces.java
@@ -1,5 +1,24 @@
 class Solution {
     
+    /*
+    
+    1 1 0 0 0 0
+
+    1 1 0 0 0 0
+
+    0 0 1 1 1 0
+
+    0 0 1 1 0 0
+
+    0 0 1 0 1 0
+
+    0 0 0 0 0 1
+    
+    visited = [t, t, f, f, f, f]
+    count = 0
+    
+    */
+    
     public int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
     public int findCircleNum(int[][] isConnected) {
         if (isConnected == null || isConnected.length == 0)
@@ -16,11 +35,11 @@ class Solution {
         return count;
     }
     
-    public void dfs(int[][] isConnected, boolean[] visited, int node) {
-        for (int i = 0; i < isConnected.length; i++) {
-            if (isConnected[node][i] == 1 && visited[i] == false) {
-                visited[i] = true;
-                dfs(isConnected, visited, i);
+    public void dfs(int[][] isConnected, boolean[] visited, int row) {
+        for (int col = 0; col < isConnected.length; col++) {
+            if (isConnected[row][col] == 1 && visited[col] == false) {
+                visited[col] = true;
+                dfs(isConnected, visited, col);
             }
         }
     }
