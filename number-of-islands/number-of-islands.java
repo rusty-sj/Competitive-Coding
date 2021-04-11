9ce200580d3cc92fb2790bfb42f69d14997d3c57
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-04-11 00:51:57 -0700

    Time: 92 ms (5.68%), Space: 42 MB (11.82%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,37 +1,62 @@
-class Solution {
+class DSU {
+    int[] parents;
     
-    public int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0)
-            return 0;
-        
-        int nIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
-                    dfs(grid, i, j);
-                    nIslands++;
-                }
-            }
-        }
-        return nIslands;
+    public DSU(int n) {
+        parents = new int[n];
+        for (int i = 0; i < n; i++) parents[i] = i;
     }
     
-    public void dfs(char[][] grid, int i, int j) {
-        
-        // Mark visited
-        grid[i][j] = '#';
-        
-        // Recursive step
-        for (int[] dir: dirs) {
-            int row = dir[0] + i;
-            int column = dir[1] + j;
-            
-            if (row >= 0 && row < grid.length
-               && column >= 0 && column < grid[i].length 
-               && grid[row][column] == '1')
-                dfs(grid, row, column);
-            
-        }
+    public int findParent(int i) {
+        if (parents[i] != i) 
+            parents[i] = findParent(parents[i]);
+        return parents[i];
+    }
+    
+    public void parentPrinter() {
+        for (int i = 0; i < parents.length; i++) System.out.print(parents[i] + " ");
+    }
+    
+    public boolean union(int i, int j) {
+        int parent1 = findParent(i), parent2 = findParent(j);
+        if (parent1 == parent2)
+            return false;
+        parents[parent1] = parent2;
+        return true;
     }
 }
+
+class Solution {
+    
+    int[][] dirs = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int numIslands(char[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        DSU dsu = new DSU(m * n);
+        int components = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    components++;
+                    for (int[] dir : dirs) {
+                        int row = i + dir[0];
+                        int col = j + dir[1];
+                        if (row >= 0 && row < m &&
+                           col >= 0 && col < n && grid[row][col] == '1')
+                            if (dsu.union(i * n + j, row * n + col))
+                                components--;
+                    }
+                } 
+            }
+        }
+        dsu.parentPrinter();
+        return components;
+    }
+}
+
+/*
+(0,0) (0,1) (0,2)
+0       1       2
+(1,0) (1,1) (1,2)
+3       4       5
+(2,0) (2,1) (2,2)
+6       7       8
+*/
