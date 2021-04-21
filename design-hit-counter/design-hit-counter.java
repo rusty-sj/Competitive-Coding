f81234169a3072fac8a0b3dde1c3453298b14227
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-04-20 22:03:52 -0700

    Time: 0 ms (100.00%), Space: 37.3 MB (19.95%) - LeetHub

---
+++ design-hit-counter/design-hit-counter.java
@@ -0,0 +1,31 @@
+class HitCounter {
+
+    Queue<Integer> hits;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        this.hits = new LinkedList<Integer>();
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        this.hits.add(timestamp);
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        int threshold = timestamp - 300;
+        while (!this.hits.isEmpty() && this.hits.peek() <= threshold) {
+            this.hits.poll();
+        }
+        return this.hits.size();
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
