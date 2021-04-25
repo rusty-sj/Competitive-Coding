0b123d6d8692fca23e054cf2650dd3da9399bdf9
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-04-25 14:18:07 -0700

    Time: 1 ms (65.72%), Space: 38.6 MB (50.10%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,16 +1,19 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0)
-            return 0;
-        int profit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            while (i < prices.length - 1 && prices[i] > prices[i + 1])
-                i++;
-            profit -= prices[i];
-            while (i < prices.length - 1 && prices[i] < prices[i + 1])
-                i++;
-            profit += prices[i];
-        }
-        return profit;
-    }
+    
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int i = 0;
+        
+        while (i < prices.length - 1) {
+            while (i < prices.length - 1 && prices[i] > prices[i + 1])
+                i++;
+            
+            profit -= prices[i];
+            while (i < prices.length - 1 && prices[i] < prices[i + 1])
+                i++;
+            profit += prices[i];
+            i++;
+        }
+        return profit;
+    }
 }
