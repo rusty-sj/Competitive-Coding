defa03c94f7821f57b663cddc3981e8c23e6d889
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-04-28 17:39:57 -0700

    Time: 28 ms (18.81%), Space: 49.4 MB (5.64%) - LeetHub

--- logger-rate-limiter/logger-rate-limiter.java
+++ logger-rate-limiter/logger-rate-limiter.java
@@ -1,20 +1,38 @@
+class Message {
+    String message;
+    int timestamp;
+    
+    public Message(String message, int timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+}
+
 class Logger {
 
-    Map<String, Integer> messageMap;
+    Queue<Message> queue;
+    Set<String> set;
     
     /** Initialize your data structure here. */
     public Logger() {
-        this.messageMap = new HashMap<>();
+        this.queue = new LinkedList<>();
+        this.set = new HashSet<>();
     }
     
     /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
         If this method returns false, the message will not be printed.
         The timestamp is in seconds granularity. */
     public boolean shouldPrintMessage(int timestamp, String message) {
-        if (this.messageMap.getOrDefault(message, 0) > timestamp) {
-                return false;
+        while (!this.queue.isEmpty() && this.queue.peek().timestamp <= timestamp - 10) {
+            Message toPoll = this.queue.poll();
+            this.set.remove(toPoll.message);
         }
-        this.messageMap.put(message, timestamp + 10);
+        if (this.set.contains(message))
+            return false;
+        else {
+            this.queue.add(new Message(message, timestamp));
+            this.set.add(message);
+        }
         return true;
     }
 }
