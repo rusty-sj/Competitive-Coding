5190e4a261a203925e621254baa695865089b261
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-05-25 10:41:40 -0700

    Time: 1 ms (22.52%), Space: 38.2 MB (18.85%) - LeetHub

--- symmetric-tree/symmetric-tree.java
+++ symmetric-tree/symmetric-tree.java
@@ -14,23 +14,28 @@
  * }
  */
 class Solution {
-    
     public boolean isSymmetric(TreeNode root) {
         if (root == null)
             return true;
-        return recursion(root.left, root.right);
-    }
-    
-    public boolean recursion(TreeNode node1, TreeNode node2) {
-        if (node1 == null && node2 == null) {
-            return true;
-        }
-        if (node1 == null || node2 == null) {
-            return false;
-        }
-        if (node1 != null && node2 != null && node1.val != node2.val)
-            return false;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
         
-        return recursion(node1.left, node2.right) && recursion(node1.right, node2.left);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.poll(), node2 = queue.poll();
+        
+            if (node1 == null && node2 == null)         
+                continue;
+            else if (node1 == null || node2 == null)
+                return false;
+            else if (node1.val != node2.val)
+                return false;
+            
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
     }
 }
