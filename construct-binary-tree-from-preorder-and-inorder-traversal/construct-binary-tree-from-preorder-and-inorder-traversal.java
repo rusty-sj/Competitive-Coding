9fd05e84e44e7440504e37b506c80133273fe65c
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-05-26 09:18:32 -0700

    Time: 8 ms (7.65%), Space: 88.8 MB (5.17%) - LeetHub

--- construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,14 +17,17 @@ class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         if (preorder == null || preorder.length == 0)
             return null;
-        int splitpoint = 0;
-        for (int i = 0; i < inorder.length; i++) {
-            if (inorder[i] == preorder[0])
-                splitpoint = i;
-        }
+        
         TreeNode root = new TreeNode(preorder[0]);
-        root.left = buildTree(Arrays.copyOfRange(preorder, 1, splitpoint + 1), Arrays.copyOfRange(inorder, 0, splitpoint));
-        root.right = buildTree(Arrays.copyOfRange(preorder, splitpoint + 1, preorder.length), Arrays.copyOfRange(inorder, splitpoint + 1, preorder.length));
+        int splitIndex = -1;
+        for (int i = 0; i < inorder.length; i++) {
+            if (preorder[0] == inorder[i]) {
+                splitIndex = i;
+                break;
+            }
+        }
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, splitIndex + 1), Arrays.copyOfRange(inorder, 0, splitIndex));
+        root.right = buildTree(Arrays.copyOfRange(preorder, splitIndex + 1, preorder.length), Arrays.copyOfRange(inorder, splitIndex + 1, inorder.length));
         return root;
     }
 }
