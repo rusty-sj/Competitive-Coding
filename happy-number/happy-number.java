b28235f65dd196aaa34146c6efd95592ff12bf0c
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-06-08 23:37:50 -0700

    Time: 3 ms (12.59%), Space: 37.9 MB (13.12%) - LeetHub

--- happy-number/happy-number.java
+++ happy-number/happy-number.java
@@ -1,28 +1,26 @@
 class Solution {
     public boolean isHappy(int n) {
+        
         Set<Integer> set = new HashSet<>();
         
-        int sum = getDigitSquaresSum(n);
-        
+        int sum = n;
         while (true) {
-            if (set.contains(sum))
+            sum = getDigitSquaresSum(sum);
+            if (set.contains(sum))  // Found cycle
                 return false;
             set.add(sum);
             if (sum == 1)
                 return true;
-            sum = getDigitSquaresSum(sum);
         }
     }
     
     public int getDigitSquaresSum(int n) {
         int sum = 0;
-        
-        while (n != 0) {
+        while (n > 0) {
             int mod = n % 10;
             sum += mod * mod;
             n = n / 10;
-        }
-        
+        } 
         return sum;
     }
 }
