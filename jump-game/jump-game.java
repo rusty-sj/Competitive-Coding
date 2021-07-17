f7b876322653ef84c9ee44998673b0d885c77b43
Rashmi Jadhav <rsjcoepit@gmail.com>
2021-07-17 15:48:15 -0700

    Time: 90 ms (26.72%), Space: 39.8 MB (70.89%) - LeetHub

--- jump-game/jump-game.java
+++ jump-game/jump-game.java
@@ -1,39 +1,38 @@
 class Solution {
-    
-    // SC: O(2n)
-    // TC: O(max_array_value^size_input)
-    public boolean[] dp;       // Store explored answers dp[i] = true
-    public boolean[] visited;  // Mark visited if explored
-    
-    
-    // [2,3,1,3,4]
-    // [0,1,2,3,4]
     public boolean canJump(int[] nums) {
-        dp = new boolean[nums.length];
-        visited = new boolean[nums.length];
-        
-        return recursion(nums, 0);
+        boolean[] dp = new boolean[nums.length];
+        dp[nums.length - 1] = true;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            // i -> current index
+            for (int step = 1; step <= nums[i]; step++) {
+                // take j steps from i
+                if (dp[i + step] == true) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
     
-    public boolean recursion(int[] nums, int index) {
-        
-        // If already explored, take answer from dp array
-        if (visited[index] == true)  
-            return dp[index];
-        
-        // Base case: if reached the last index, return true
-        if (index == nums.length - 1)
-            return true;
-        
-        // Explore all possible steps from index
-        for (int i = 1; i <= nums[index]; i++)
-            if (recursion(nums, index + i)) { 
-                dp[index] = true;
-                return true;
-            } 
-        // Done exploring steps for index, mark visited
-        visited[index] = true;
-        
-        return false;
-    }
+    
+    /* 3 2 1 0 4
+       0 1 2 3 4
+       
+          0
+       /  |   \
+     1    2    3 
+    /\    |    
+   2  3   3
+   |
+   3
+   
+  index  0 1 2 3 4 
+  array  2 3 1 1 4
+  
+         T T T T T   
+            
+  
+    
+    */
 }
